d78d8ee44eb1d3f263b08cf0796e9723bc2e618a
Yonas Fayera <yonasfayera05@gmail.com>
2026-01-10 21:33:42 +0300

    modules/rds/variables.tf definition

---
+++ modules/rds/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name"            { type = string }
+variable "vpc_id"                  { type = string }
+variable "private_subnet_ids"      { type = list(string) }
+variable "ecs_sg_id"               { type = string }
+variable "db_engine"               { type = string }
+variable "db_instance_class"       { type = string }
+variable "db_name"                 { type = string }
+variable "db_username"             { type = string }
+variable "db_password_secret_name" { type = string }
